a56826a3ae72a4b7191ba07d2954793ce3cb8813
Dennis Daume <daume.dennis@gmail.com>
2012-02-02 20:01:53 +0100

    The input mask now disappears, after the administrator has been created

--- Espera/Espera.View/ViewModels/AdministratorViewModel.cs
+++ Espera/Espera.View/ViewModels/AdministratorViewModel.cs
@@ -4,11 +4,16 @@ using FlagLib.Patterns.MVVM;
 
 namespace Espera.View.ViewModels
 {
-    public class AdministratorViewModel
+    public class AdministratorViewModel : ViewModelBase<AdministratorViewModel>
     {
         private readonly Library library;
 
         public string CreationPassword { get; set; }
+
+        public bool IsAdminCreated
+        {
+            get { return this.library.IsAdministratorCreated; }
+        }
 
         public ICommand CreateAdminCommand
         {
@@ -16,7 +21,12 @@ namespace Espera.View.ViewModels
             {
                 return new RelayCommand
                 (
-                    param => this.library.CreateAdmin(this.CreationPassword)
+                    param =>
+                    {
+                        this.library.CreateAdmin(this.CreationPassword);
+                        this.OnPropertyChanged(vm => vm.IsAdminCreated);
+                    },
+                    param => !string.IsNullOrWhiteSpace(this.CreationPassword)
                 );
             }
         }
