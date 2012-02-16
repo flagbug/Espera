c05687f4e9b6e6b3139ec6b0e9685bea5bff174d
Dennis Daume <daume.dennis@gmail.com>
2012-02-16 21:45:12 +0100

    Fixed the namespace

--- Espera/Espera.View/SearchEngine.cs
+++ Espera/Espera.View/SearchEngine.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Espera.Core;
 using FlagLib.Reflection;
 
-namespace Espera.Core
+namespace Espera.View
 {
     public static class SearchEngine
     {
