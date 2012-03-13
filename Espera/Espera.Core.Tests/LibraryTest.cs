e0b5792832f1fe31ad6f39d4e56ca27729ad7e18
Dennis Daume <daume.dennis@gmail.com>
2012-03-13 11:59:56 +0100

    Added tests for the Library class

---
+++ Espera/Espera.Core.Tests/LibraryTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Espera.Core.Tests
+{
+    [TestClass]
+    public class LibraryTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CreateAdmin_PasswordIsNull_ThrowsArgumentNullException()
+        {
+            var library = new Library.Library();
+            library.CreateAdmin(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateAdmin_PasswordIsEmpty_ThrowsArgumentException()
+        {
+            var library = new Library.Library();
+            library.CreateAdmin(String.Empty);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void CreateAdmin_PasswordIsWhitespace_ThrowsArgumentException()
+        {
+            var library = new Library.Library();
+            library.CreateAdmin(" ");
+        }
+
+        [TestMethod]
+        public void CreateAdmin_PasswordIsTestPassword_AdministratorIsCreated()
+        {
+            var library = new Library.Library();
+            library.CreateAdmin("TestPassword");
+
+            Assert.IsTrue(library.IsAdministratorCreated);
+        }
+    }
+}
