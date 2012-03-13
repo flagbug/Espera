ee6481a90d68d1614549af154ee65bf0a61a59a7
Dennis Daume <daume.dennis@gmail.com>
2012-03-13 11:36:12 +0100

    Refactored the test code

--- Espera/Espera.Core.Tests/PlaylistTest.cs
+++ Espera/Espera.Core.Tests/PlaylistTest.cs
@@ -12,63 +12,62 @@ namespace Espera.Core.Tests
     [TestClass]
     public class PlaylistTest
     {
-        private readonly Mock<Song> song = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero);
-
-        private readonly Mock<Song> song1 = new Mock<Song>("TestPath1", AudioType.Mp3, TimeSpan.Zero);
-
-        private readonly Mock<Song> song2 = new Mock<Song>("TestPath2", AudioType.Mp3, TimeSpan.Zero);
-
-        private readonly Mock<Song> song3 = new Mock<Song>("TestPath3", AudioType.Mp3, TimeSpan.Zero);
-
-        public PlaylistTest()
-        {
-            this.song.Setup(p => p.LoadToCache()).Callback(() => Thread.Sleep(1000));
-
-            this.song1.Setup(p => p.LoadToCache()).Callback(() => Thread.Sleep(2000));
-
-            this.song2.Setup(p => p.LoadToCache()).Callback(() => Thread.Sleep(3000));
-
-            this.song3.Setup(p => p.LoadToCache()).Callback(() => Thread.Sleep(4000));
-        }
-
-        private Playlist SetupPlaylist()
+        private static Playlist SetupPlaylist(IEnumerable<Song> songs)
         {
             var playlist = new Playlist();
 
-            playlist.AddSongs(new List<Song> { this.song.Object, this.song1.Object, this.song2.Object, this.song3.Object });
+            playlist.AddSongs(songs);
 
             return playlist;
+        }
+
+        private static Song[] SetupSongs(int count)
+        {
+            var songs = new Song[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                var mock = new Mock<Song>("TestPath" + count, AudioType.Mp3, TimeSpan.Zero);
+                mock.Setup(p => p.LoadToCache()).Callback(() => Thread.Sleep(3000));
+
+                songs[i] = mock.Object;
+            }
+
+            return songs;
         }
 
         [TestMethod]
         public void AddSongsTest()
         {
-            Playlist playlist = this.SetupPlaylist();
+            Song[] songs = SetupSongs(4);
+            Playlist playlist = SetupPlaylist(songs);
 
             Assert.AreEqual(4, playlist.Count());
-            Assert.AreEqual(this.song.Object, playlist[0]);
-            Assert.AreEqual(this.song1.Object, playlist[1]);
-            Assert.AreEqual(this.song2.Object, playlist[2]);
-            Assert.AreEqual(this.song3.Object, playlist[3]);
+            Assert.AreEqual(songs[0], playlist[0]);
+            Assert.AreEqual(songs[1], playlist[1]);
+            Assert.AreEqual(songs[2], playlist[2]);
+            Assert.AreEqual(songs[3], playlist[3]);
         }
 
         [TestMethod]
         public void RemoveSongs_RemoveOneSong_OrderIsCorrect()
         {
-            Playlist playlist = this.SetupPlaylist();
+            Song[] songs = SetupSongs(4);
+            Playlist playlist = SetupPlaylist(songs);
 
             playlist.RemoveSongs(new List<int> { 1 });
 
             Assert.AreEqual(3, playlist.Count());
-            Assert.AreEqual(this.song.Object, playlist[0]);
-            Assert.AreEqual(this.song2.Object, playlist[1]);
-            Assert.AreEqual(this.song3.Object, playlist[2]);
+            Assert.AreEqual(songs[0], playlist[0]);
+            Assert.AreEqual(songs[2], playlist[1]);
+            Assert.AreEqual(songs[3], playlist[2]);
         }
 
         [TestMethod]
         public void CanPlayNextSong_CurrentSongIndexIsZero_ReturnsTrue()
         {
-            Playlist playlist = this.SetupPlaylist();
+            Song[] songs = SetupSongs(4);
+            Playlist playlist = SetupPlaylist(songs);
 
             playlist.CurrentSongIndex = 0;
 
@@ -78,7 +77,8 @@ namespace Espera.Core.Tests
         [TestMethod]
         public void CanPlayNextSong_CurrentSongIndexIsPlaylistCount_ReturnsFalse()
         {
-            Playlist playlist = this.SetupPlaylist();
+            Song[] songs = SetupSongs(4);
+            Playlist playlist = SetupPlaylist(songs);
 
             playlist.CurrentSongIndex = playlist.Count();
 
@@ -88,7 +88,8 @@ namespace Espera.Core.Tests
         [TestMethod]
         public void CanPlayNextSong_CurrentSongIndexIsNull_ReturnsFalse()
         {
-            Playlist playlist = this.SetupPlaylist();
+            Song[] songs = SetupSongs(4);
+            Playlist playlist = SetupPlaylist(songs);
 
             playlist.CurrentSongIndex = null;
 
@@ -98,7 +99,8 @@ namespace Espera.Core.Tests
         [TestMethod]
         public void CanPlayPreviousSong_CurrentSongIndexIsZero_ReturnsFalse()
         {
-            Playlist playlist = this.SetupPlaylist();
+            Song[] songs = SetupSongs(4);
+            Playlist playlist = SetupPlaylist(songs);
 
             playlist.CurrentSongIndex = 0;
 
@@ -108,7 +110,8 @@ namespace Espera.Core.Tests
         [TestMethod]
         public void CanPlayPreviousSong_CurrentSongIndexIsPlaylistCount_ReturnsTrue()
         {
-            Playlist playlist = this.SetupPlaylist();
+            Song[] songs = SetupSongs(4);
+            Playlist playlist = SetupPlaylist(songs);
 
             playlist.CurrentSongIndex = playlist.Count();
 
@@ -118,7 +121,8 @@ namespace Espera.Core.Tests
         [TestMethod]
         public void CanPlayPreviousSong_CurrentSongIndexIsNull_ReturnsFalse()
         {
-            Playlist playlist = this.SetupPlaylist();
+            Song[] songs = SetupSongs(4);
+            Playlist playlist = SetupPlaylist(songs);
 
             playlist.CurrentSongIndex = null;
 
