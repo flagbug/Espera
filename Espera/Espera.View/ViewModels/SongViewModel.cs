3fabed9883fc215088af4a9e1848c115a411cea9
Dennis Daume <daume.dennis@gmail.com>
2012-06-17 23:10:00 +0200

    Async thumbnail loading

--- Espera/Espera.View/ViewModels/SongViewModel.cs
+++ Espera/Espera.View/ViewModels/SongViewModel.cs
@@ -1,4 +1,8 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -32,10 +36,7 @@ namespace Espera.View.ViewModels
 
         public ICommand OpenPathCommand
         {
-            get
-            {
-                return new RelayCommand(param => Process.Start(this.Path));
-            }
+            get { return new RelayCommand(param => Process.Start(this.Path)); }
         }
 
         public string Path
@@ -59,10 +60,74 @@ namespace Espera.View.ViewModels
             {
                 var song = this.Model as YoutubeSong;
 
-                return song == null
-                           ? null
-                           : (this.thumbnail ?? (this.thumbnail = new BitmapImage(song.ThumbnailSource)));
+                if (song == null)
+                    return null;
+
+                if (this.thumbnail == null)
+                {
+                    this.GetThumbnail();
+                }
+
+                return this.thumbnail;
             }
+        }
+
+        private void GetThumbnail()
+        {
+            var worker = new BackgroundWorker();
+
+            worker.DoWork += (s, e) =>
+            {
+                var uri = e.Argument as Uri;
+
+                using (var webClient = new WebClient())
+                {
+                    try
+                    {
+                        byte[] imageBytes = webClient.DownloadData(uri);
+
+                        if (imageBytes == null)
+                        {
+                            e.Result = null;
+                            return;
+                        }
+
+                        var imageStream = new MemoryStream(imageBytes);
+                        var image = new BitmapImage();
+
+                        image.BeginInit();
+                        image.StreamSource = imageStream;
+                        image.CacheOption = BitmapCacheOption.OnLoad;
+                        image.EndInit();
+
+                        image.Freeze();
+
+                        imageStream.Close();
+
+                        e.Result = image;
+                    }
+
+                    catch (WebException ex)
+                    {
+                        e.Result = ex;
+                    }
+                }
+            };
+
+            worker.RunWorkerCompleted += (s, e) =>
+            {
+                var bitmapImage = e.Result as BitmapImage;
+
+                if (bitmapImage != null)
+                {
+                    this.thumbnail = bitmapImage;
+                    this.OnPropertyChanged(vm => vm.Thumbnail);
+                }
+
+                worker.Dispose();
+            };
+
+            worker.RunWorkerAsync(((YoutubeSong)this.Model).ThumbnailSource);
         }
     }
 }
