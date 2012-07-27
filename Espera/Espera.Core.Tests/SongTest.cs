b91cf8206c3b7954560d3b81e00af179e22e5015
Dennis Daume <daume.dennis@gmail.com>
2012-07-27 20:27:08 +0200

    Refactorings

--- Espera/Espera.Core.Tests/SongTest.cs
+++ Espera/Espera.Core.Tests/SongTest.cs
@@ -20,42 +20,42 @@ namespace Espera.Core.Tests
         [Test]
         public void EqualsNullIsFalse()
         {
-            var song = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero);
+            var song = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero).Object;
 
-            Assert.IsFalse(song.Object.Equals(null));
+            Assert.IsFalse(song.Equals(null));
         }
 
         [Test]
         public void EqualsSamePathIsTrue()
         {
-            var song1 = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero);
-            var song2 = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero);
+            var song1 = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero).Object;
+            var song2 = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero).Object;
 
-            Assert.IsTrue(song1.Object.Equals(song2.Object));
+            Assert.IsTrue(song1.Equals(song2));
         }
 
         [Test]
         public void EqualsSameReferenceIsTrue()
         {
-            var song = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero);
+            var song = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero).Object;
 
-            Assert.IsTrue(song.Object.Equals(song.Object));
+            Assert.IsTrue(song.Equals(song));
         }
 
         [Test]
         public void EqualsSongWithDifferentPathIsFalse()
         {
-            var song1 = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero);
-            var song2 = new Mock<Song>("TestPath1", AudioType.Mp3, TimeSpan.Zero);
+            var song1 = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero).Object;
+            var song2 = new Mock<Song>("TestPath1", AudioType.Mp3, TimeSpan.Zero).Object;
 
-            Assert.IsFalse(song1.Object.Equals(song2.Object));
+            Assert.IsFalse(song1.Equals(song2));
         }
 
         [Test]
         public void GetHashcode_EqualObjects_ReturnsSameHashCode()
         {
-            var song1 = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero) { CallBase = true };
-            var song2 = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero) { CallBase = true };
+            var song1 = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero) { CallBase = true }.Object;
+            var song2 = new Mock<Song>("TestPath", AudioType.Mp3, TimeSpan.Zero) { CallBase = true }.Object;
 
             Assert.AreEqual(song1.GetHashCode(), song2.GetHashCode());
         }
