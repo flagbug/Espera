f9fdded4235eddbf0bb849884dd75312d666085d
Dennis Daume <daume.dennis@gmail.com>
2012-07-27 20:07:14 +0200

    Some simple unit tests for audio players

---
+++ Espera/Espera.Core.Tests/AudioPlayerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Espera.Core.Audio;
+using Moq;
+using NUnit.Framework;
+
+namespace Espera.Core.Tests
+{
+    [TestFixture]
+    public class AudioPlayerTest
+    {
+        [Test]
+        public void Load_ArgumentIsNull_ThrowsArgumentNullException()
+        {
+            var audioPlayer = new Mock<AudioPlayer> { CallBase = true }.Object;
+
+            Assert.Throws<ArgumentNullException>(() => audioPlayer.Load(null));
+        }
+    }
+}
---
+++ Espera/Espera.Core.Tests/LocalAudioPlayerTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Espera.Core.Audio;
+using NUnit.Framework;
+
+namespace Espera.Core.Tests
+{
+    [TestFixture]
+    public class LocalAudioPlayerTest
+    {
+        [Test]
+        public void CurrentTime_NoSongLoaded_ReturnsTimeSpandZero()
+        {
+            var audioPlayer = new LocalAudioPlayer();
+
+            Assert.AreEqual(TimeSpan.Zero, audioPlayer.CurrentTime);
+        }
+
+        [Test]
+        public void PlaybackState_NoSongLoaded_ReturnsNone()
+        {
+            var audioPlayer = new LocalAudioPlayer();
+
+            Assert.AreEqual(AudioPlayerState.None, audioPlayer.PlaybackState);
+        }
+
+        [Test]
+        public void TotalTime_NoSongLoaded_ReturnsTimeSpanZero()
+        {
+            var audioPlayer = new LocalAudioPlayer();
+
+            Assert.AreEqual(TimeSpan.Zero, audioPlayer.TotalTime);
+        }
+
+        [Test]
+        public void Volume_NoSongLoadedAndVolumeIsSet_ReturnsSettedVolume()
+        {
+            var audioPlayer = new LocalAudioPlayer { Volume = 0.5f };
+
+            Assert.AreEqual(0.5f, audioPlayer.Volume);
+        }
+
+        [Test]
+        public void Load_ArgumentIsNull_ThrowsArgumentNullException()
+        {
+            var audioPlayer = new LocalAudioPlayer();
+
+            Assert.Throws<ArgumentNullException>(() => audioPlayer.Load(null));
+        }
+    }
+}
