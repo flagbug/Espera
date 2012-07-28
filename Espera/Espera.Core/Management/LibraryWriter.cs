728375bfd814ded9c3670dd61761fa8e1e551388
Dennis Daume <daume.dennis@gmail.com>
2012-07-28 17:59:46 +0200

    Some thoughts about how to store the library

---
+++ Espera/Espera.Core/Management/LibraryWriter.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace Espera.Core.Management
+{
+    internal class LibraryWriter
+    {
+        public static void Write(Library library)
+        {
+            var document = new XDocument(
+                new XElement("Root",
+                    new XElement("Version", "1.0.0"),
+                    new XElement("Songs", library.Songs.Select(song =>
+                        new XElement("Song",
+                            new XAttribute("Album", song.Album),
+                            new XAttribute("Artist", song.Artist),
+                            new XAttribute("AudioType", song.AudioType),
+                            new XAttribute("Duration", song.Duration.Ticks),
+                            new XAttribute("Genre", song.Genre),
+                            new XAttribute("Path", song.OriginalPath),
+                            new XAttribute("Title", song.Title),
+                            new XAttribute("TrackNumber", song.TrackNumber)))),
+                    new XElement("Playlists", library.Playlists.Select(playlist =>
+                        new XElement("Playlist",
+                            new XAttribute("Name", playlist.Name),
+                            new XElement("Songs", playlist.Songs.Select(song =>
+                                new XElement("Path", song.OriginalPath))))))));
+
+            document.Save("library.xml");
+        }
+    }
+}
