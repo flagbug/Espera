56193c79900ab2b84705d6358baf354cad2861ce
Dennis Daume <daume.dennis@gmail.com>
2012-07-29 09:56:58 +0200

    Implemented unit test for LibraryWriter.Write

---
+++ Espera/Espera.Core.Tests/LibraryWriterTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using Espera.Core.Management;
+using NUnit.Framework;
+
+namespace Espera.Core.Tests
+{
+    [TestFixture]
+    public class LibraryWriterTest
+    {
+        [Test]
+        public void Write()
+        {
+            using (Stream targetStream = new MemoryStream())
+            {
+                var songs = new[] { Helpers.LocalSong1, Helpers.LocalSong2 };
+
+                var playlists = new[] { new PlaylistInfo(Helpers.Playlist1), new PlaylistInfo(Helpers.Playlist2) };
+
+                LibraryWriter.Write(songs, playlists, targetStream);
+
+                string expected = Helpers.GenerateSaveFile();
+                string actual = StreamToString(targetStream).Replace("\r\n", String.Empty);
+
+                Assert.AreEqual(expected, actual);
+            }
+        }
+
+        private static string StreamToString(Stream stream)
+        {
+            using (var reader = new StreamReader(stream))
+            {
+                stream.Position = 0;
+
+                return reader.ReadToEnd();
+            }
+        }
+    }
+}
--- Espera/Espera.Core/Management/LibraryWriter.cs
+++ Espera/Espera.Core/Management/LibraryWriter.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Xml.Linq;
 
@@ -6,12 +7,12 @@ namespace Espera.Core.Management
 {
     internal class LibraryWriter
     {
-        public static void Write(Library library, Stream targetStream)
+        public static void Write(IEnumerable<LocalSong> songs, IEnumerable<PlaylistInfo> playlists, Stream targetStream)
         {
             var document = new XDocument(
                 new XElement("Root",
                     new XElement("Version", "1.0.0"),
-                    new XElement("Songs", library.Songs.Select(song =>
+                    new XElement("Songs", songs.Select(song =>
                         new XElement("Song",
                             new XAttribute("Album", song.Album),
                             new XAttribute("Artist", song.Artist),
@@ -21,7 +22,7 @@ namespace Espera.Core.Management
                             new XAttribute("Path", song.OriginalPath),
                             new XAttribute("Title", song.Title),
                             new XAttribute("TrackNumber", song.TrackNumber)))),
-                    new XElement("Playlists", library.Playlists.Select(playlist =>
+                    new XElement("Playlists", playlists.Select(playlist =>
                         new XElement("Playlist",
                             new XAttribute("Name", playlist.Name),
                             new XElement("Songs", playlist.Songs.Select(song =>
