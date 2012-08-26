18d0851d2fdc35322241c24aa734fc709f65d63c
Dennis Daume <daume.dennis@gmail.com>
2012-08-26 22:27:50 +0200

    Release of version 1.2.0

--- Espera/Espera.Core/Properties/AssemblyInfo.cs
+++ Espera/Espera.Core/Properties/AssemblyInfo.cs
@@ -15,12 +15,15 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleTo("Espera.Core.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // Moq
+
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("76317ba5-a287-4835-9de4-801aedb88ac1")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
@@ -31,5 +34,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
