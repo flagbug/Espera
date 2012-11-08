ac362c5c745f5b94747201c3b0962971e3133d84
Dennis Daume <daume.dennis@gmail.com>
2012-11-08 20:13:01 +0100

    Removed unnecessary comment

--- Espera/Espera.View/ViewModels/ShellViewModel.cs
+++ Espera/Espera.View/ViewModels/ShellViewModel.cs
@@ -605,9 +605,6 @@ namespace Espera.View.ViewModels
 
         public YoutubeViewModel YoutubeViewModel { get; private set; }
 
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
         public void Dispose()
         {
             Settings.Default.Save();
