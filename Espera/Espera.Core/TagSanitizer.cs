ddf29f6394097dc8dde0187d90fa5c5982e7aaa6
Dennis Daume <daume.dennis@gmail.com>
2012-12-29 23:33:25 +0100

    Sanitize the tags when reading them from a file, because when we save it to XML, some characters are invalid

---
+++ Espera/Espera.Core/TagSanitizer.cs
@@ -0,0 +1,28 @@
+﻿using Rareform.Validation;
+using System.Text;
+using System.Xml;
+
+namespace Espera.Core
+{
+    /// <summary>
+    /// Provides a method to sanitize the tag of a song (e.g removing invalid characters).
+    /// This applies not to the song on the physical drive directly, but only to the presentation in the application.
+    /// </summary>
+    internal static class TagSanitizer
+    {
+        public static string Sanitize(string tag)
+        {
+            if (tag == null)
+                Throw.ArgumentNullException(() => tag);
+
+            var buffer = new StringBuilder(tag.Length);
+
+            foreach (char c in tag)
+            {
+                buffer.Append(XmlConvert.IsXmlChar(c) ? c : '_');
+            }
+
+            return buffer.ToString();
+        }
+    }
+}
