6b5dc7ec47a18a472987669675372acacbd49fae
Dennis Daume <daume.dennis@gmail.com>
2013-09-26 17:21:30 +0200

    Unit tests

--- Espera/Espera.Services.Tests/MobileClientTest.cs
+++ Espera/Espera.Services.Tests/MobileClientTest.cs
@@ -4,6 +4,7 @@ using Moq;
 using ReactiveSockets;
 using System;
 using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -12,6 +13,35 @@ namespace Espera.Services.Tests
     public class MobileClientTest
     {
         [Fact]
+        public async Task CurrentIndexChangeFiresPushMessage()
+        {
+            var sender = new AsyncSubject<byte[]>();
+
+            var socket = new Mock<IReactiveSocket>();
+            socket.SetupGet(x => x.Receiver).Returns(Observable.Never<byte>());
+            socket.Setup(x => x.SendAsync(It.IsAny<byte[]>())).Returns(Task.Delay(0)).Callback<byte[]>(x =>
+            {
+                sender.OnNext(x);
+                sender.OnCompleted();
+            });
+
+            using (Library library = Helpers.CreateLibraryWithPlaylist())
+            {
+                library.AudioPlayerCallback.PlayRequest = () => { };
+                library.AddSongToPlaylist(Helpers.SetupSongMock());
+
+                using (var client = new MobileClient(socket.Object, library))
+                {
+                    client.ListenAsync();
+
+                    await library.PlaySongAsync(0);
+
+                    await sender.Timeout(TimeSpan.FromSeconds(5));
+                }
+            }
+        }
+
+        [Fact]
         public async Task FiresDisconnectOnSocketDisconnect()
         {
             var socket = new Mock<IReactiveSocket>();
@@ -19,14 +49,15 @@ namespace Espera.Services.Tests
 
             using (Library library = Helpers.CreateLibraryWithPlaylist())
             {
-                var client = new MobileClient(socket.Object, library);
+                using (var client = new MobileClient(socket.Object, library))
+                {
+                    var conn = client.Disconnected.FirstAsync().PublishLast();
+                    conn.Connect();
 
-                var conn = client.Disconnected.FirstAsync().PublishLast();
-                conn.Connect();
+                    socket.Raise(x => x.Disconnected += null, EventArgs.Empty);
 
-                socket.Raise(x => x.Disconnected += null, EventArgs.Empty);
-
-                await conn.Timeout(TimeSpan.FromSeconds(5));
+                    await conn.Timeout(TimeSpan.FromSeconds(5));
+                }
             }
         }
 
@@ -39,14 +70,43 @@ namespace Espera.Services.Tests
 
             using (Library library = Helpers.CreateLibraryWithPlaylist())
             {
-                var client = new MobileClient(socket.Object, library);
+                using (var client = new MobileClient(socket.Object, library))
+                {
+                    var conn = client.Disconnected.FirstAsync().PublishLast();
+                    conn.Connect();
 
-                var conn = client.Disconnected.FirstAsync().PublishLast();
-                conn.Connect();
+                    client.ListenAsync();
 
-                library.AddAndSwitchToPlaylist("lolol");
+                    library.AddAndSwitchToPlaylist("lolol");
 
-                await conn.Timeout(TimeSpan.FromSeconds(5));
+                    await conn.Timeout(TimeSpan.FromSeconds(5));
+                }
+            }
+        }
+
+        [Fact]
+        public async Task PlaylistSwitchFiresPushMessage()
+        {
+            var sender = new AsyncSubject<byte[]>();
+
+            var socket = new Mock<IReactiveSocket>();
+            socket.SetupGet(x => x.Receiver).Returns(Observable.Never<byte>());
+            socket.Setup(x => x.SendAsync(It.IsAny<byte[]>())).Returns(Task.Delay(0)).Callback<byte[]>(x =>
+            {
+                sender.OnNext(x);
+                sender.OnCompleted();
+            });
+
+            using (Library library = Helpers.CreateLibraryWithPlaylist())
+            {
+                using (var client = new MobileClient(socket.Object, library))
+                {
+                    client.ListenAsync();
+
+                    library.AddAndSwitchToPlaylist("lolol");
+
+                    await sender.Timeout(TimeSpan.FromSeconds(5));
+                }
             }
         }
     }
