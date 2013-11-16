4da07783dcde18c93e1e5a97f7ad2b5840c8ac77
Dennis Daume <daume.dennis@gmail.com>
2013-11-16 15:19:00 +0100

    Expose methods for remote access

--- Espera/Espera.Core/Management/IRemoteAccessControl.cs
+++ Espera/Espera.Core/Management/IRemoteAccessControl.cs
@@ -4,7 +4,11 @@ namespace Espera.Core.Management
 {
     public interface IRemoteAccessControl
     {
+        IObservable<AccessPermission> ObserveAccessPermission(Guid accessToken);
+
         Guid RegisterRemoteAccessToken();
+
+        void SetRemotePassword(Guid accessToken, string password);
 
         void UpgradeRemoteAccess(Guid accessToken, string password);
     }
