b1a325106884ee37a95936f24f0c5ead987029aa
Dennis Daume <daume.dennis@gmail.com>
2014-02-05 17:23:12 +0100

    Display the number of votes in the UI

--- Espera/Espera.Core/Management/PlaylistEntry.cs
+++ Espera/Espera.Core/Management/PlaylistEntry.cs
@@ -1,10 +1,14 @@
 ﻿using Rareform.Validation;
 using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace Espera.Core.Management
 {
-    public class PlaylistEntry : IComparable<PlaylistEntry>
+    public class PlaylistEntry : IComparable<PlaylistEntry>, INotifyPropertyChanged
     {
+        private int votes;
+
         internal PlaylistEntry(int index, Song song)
         {
             if (index < 0)
@@ -19,13 +23,26 @@ namespace Espera.Core.Management
             this.Guid = Guid.NewGuid();
         }
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public Guid Guid { get; private set; }
 
         public int Index { get; internal set; }
 
         public Song Song { get; private set; }
 
-        public int Votes { get; private set; }
+        public int Votes
+        {
+            get { return this.votes; }
+            private set
+            {
+                if (this.votes != value)
+                {
+                    this.votes = value;
+                    this.OnPropertyChanged();
+                }
+            }
+        }
 
         public int CompareTo(PlaylistEntry other)
         {
@@ -41,6 +58,14 @@ namespace Espera.Core.Management
         internal void Vote()
         {
             Votes++;
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
