c271bad76e71a22632974fd9b23398c8992cf542
Dennis Daume <daume.dennis@gmail.com>
2014-03-14 13:45:59 +0100

    We are in beta status

--- Espera/GlobalAssemblyInfo.cs
+++ Espera/GlobalAssemblyInfo.cs
@@ -4,4 +4,4 @@
 [assembly: AssemblyCopyright("Copyright © 2013 Dennis Daume")]
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha")]
+[assembly: AssemblyInformationalVersion("2.0.0-beta")]
