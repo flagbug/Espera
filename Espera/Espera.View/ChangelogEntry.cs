497e5dceb5c736c63c107505c9794748bad1143b
Dennis Daume <daume.dennis@gmail.com>
2014-03-19 00:02:37 +0100

    Write a changelog parser for our markdown changelog

---
+++ Espera/Espera.View/ChangelogEntry.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Espera.View
+{
+    public class ChangelogEntry
+    {
+        public IReadOnlyList<string> Descriptions { get; set; }
+
+        public string Type { get; set; }
+    }
+}
