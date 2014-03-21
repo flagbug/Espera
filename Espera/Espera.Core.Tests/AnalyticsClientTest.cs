5b75f31d7309fb4768fadb554cc73fa9877b06c3
Dennis Daume <daume.dennis@gmail.com>
2014-03-21 01:01:25 +0100

    Add a bunch of unit tests

--- Espera/Espera.Core.Tests/AnalyticsClientTest.cs
+++ Espera/Espera.Core.Tests/AnalyticsClientTest.cs
@@ -88,15 +88,67 @@ namespace Espera.Core.Tests
             }
         }
 
+        public class TheRecordBugReportAsyncMethod
+        {
+            [Fact]
+            public async Task ForcesAuthentication()
+            {
+                var coreSettings = new CoreSettings { EnableAutomaticReports = false };
+                var endpoint = Substitute.For<IAnalyticsEndpoint>();
+                var client = new AnalyticsClient(endpoint);
+
+                // This won't authenticate because automatic reports are disabled
+                await client.InitializeAsync(coreSettings);
+
+                // This will force an authentication even if automatic reports are disabled
+                await client.RecordBugReportAsync("blabla");
+
+                Assert.True(client.IsAuthenticated);
+                endpoint.Received().RecordErrorAsync("blabla", Arg.Any<string>());
+            }
+
+            [Fact]
+            public async Task IgnoresEmailIfNullOrEmpty()
+            {
+                var coreSettings = new CoreSettings { EnableAutomaticReports = true };
+                var endpoint = Substitute.For<IAnalyticsEndpoint>();
+                var client = new AnalyticsClient(endpoint);
+                await client.InitializeAsync(coreSettings);
+
+                await client.RecordBugReportAsync("blabla");
+
+                endpoint.DidNotReceiveWithAnyArgs().UpdateUserEmailAsync(null);
+
+                await client.RecordBugReportAsync("blabla", String.Empty);
+
+                endpoint.DidNotReceiveWithAnyArgs().UpdateUserEmailAsync(null);
+
+                await client.RecordBugReportAsync("blabla", "  ");
+
+                endpoint.DidNotReceiveWithAnyArgs().UpdateUserEmailAsync(null);
+            }
+
+            [Fact]
+            public async Task UpdatesEmailIfSet()
+            {
+                var coreSettings = new CoreSettings { EnableAutomaticReports = true };
+                var endpoint = Substitute.For<IAnalyticsEndpoint>();
+                var client = new AnalyticsClient(endpoint);
+                await client.InitializeAsync(coreSettings);
+
+                await client.RecordBugReportAsync("blabla", "email@email.com");
+
+                endpoint.Received().UpdateUserEmailAsync("email@email.com");
+            }
+        }
+
         public class TheRecordCrashAsyncMethod
         {
             [Fact]
             public async Task ForcesAuthentication()
             {
                 var coreSettings = new CoreSettings { EnableAutomaticReports = false };
-
                 var endpoint = Substitute.For<IAnalyticsEndpoint>();
-
                 var client = new AnalyticsClient(endpoint);
 
                 // This won't authenticate because automatic reports are disabled
@@ -106,6 +158,38 @@ namespace Espera.Core.Tests
                 await client.RecordCrashAsync(new Exception());
 
                 Assert.True(client.IsAuthenticated);
+                endpoint.ReceivedWithAnyArgs().RecordErrorAsync(null, null);
+            }
+        }
+
+        public class TheRecordErrorAsyncMethod
+        {
+            [Fact]
+            public async Task RespectsDisabledReports()
+            {
+                var coreSettings = new CoreSettings { EnableAutomaticReports = false };
+                var endpoint = Substitute.For<IAnalyticsEndpoint>();
+                var client = new AnalyticsClient(endpoint);
+
+                await client.InitializeAsync(coreSettings);
+
+                await client.RecordErrorAsync(new Exception(), false);
+
+                Assert.False(client.IsAuthenticated);
+                endpoint.DidNotReceiveWithAnyArgs().RecordErrorAsync(null, null);
+            }
+
+            [Fact]
+            public async Task SendsReport()
+            {
+                var coreSettings = new CoreSettings { EnableAutomaticReports = true };
+                var endpoint = Substitute.For<IAnalyticsEndpoint>();
+                var client = new AnalyticsClient(endpoint);
+
+                await client.InitializeAsync(coreSettings);
+
+                await client.RecordErrorAsync(new Exception(), false);
+
                 endpoint.ReceivedWithAnyArgs().RecordErrorAsync(null, null);
             }
         }
