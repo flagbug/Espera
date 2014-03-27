b779e892cef0b974a84cee3eb8d2d8da9580c68c
Dennis Daume <daume.dennis@gmail.com>
2014-03-27 17:15:05 +0100

    Add a bunch of tests for the UpdateViewModel

---
+++ Espera/Espera.View.Tests/UpdateViewModelTest.cs
@@ -0,0 +1,63 @@
+﻿using Espera.View.ViewModels;
+using Xunit;
+
+namespace Espera.View.Tests
+{
+    public class UpdateViewModelTest
+    {
+        public class TheChangelogShownMethod
+        {
+            [Fact]
+            public void DisabledChangelogIfRequested()
+            {
+                var settings = new ViewSettings { IsUpdated = true, EnableChangelog = true };
+                var vm = new UpdateViewModel(settings) { DisableChangelog = true };
+
+                vm.ChangelogShown();
+
+                Assert.False(settings.EnableChangelog);
+            }
+
+            [Fact]
+            public void SetsIsUpdatedToFalse()
+            {
+                var settings = new ViewSettings { IsUpdated = true };
+                var vm = new UpdateViewModel(settings);
+
+                vm.ChangelogShown();
+
+                Assert.False(settings.IsUpdated);
+            }
+        }
+
+        public class TheShowChangelogProperty
+        {
+            [Fact]
+            public void IsFalseIfNotUpdated()
+            {
+                var settings = new ViewSettings { IsUpdated = false };
+                var vm = new UpdateViewModel(settings);
+
+                Assert.False(vm.ShowChangelog);
+            }
+
+            [Fact]
+            public void IsFalseIfUpdatedButOptedOut()
+            {
+                var settings = new ViewSettings { IsUpdated = true, EnableChangelog = false };
+                var vm = new UpdateViewModel(settings);
+
+                Assert.False(vm.ShowChangelog);
+            }
+
+            [Fact]
+            public void IsTrueIfUpdatedAndOptedIn()
+            {
+                var settings = new ViewSettings { IsUpdated = true, EnableChangelog = true };
+                var vm = new UpdateViewModel(settings);
+
+                Assert.True(vm.ShowChangelog);
+            }
+        }
+    }
+}
