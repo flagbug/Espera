9efce73750172e0c48e9b2d7db3613b1e40b9972
Dennis Daume <daume.dennis@gmail.com>
2014-03-28 11:00:53 +0100

    Create a temporary library file and rename it to reduce the chance of library corruption
    Fixes #37

--- Espera/Espera.Core/Management/LibraryFileWriter.cs
+++ Espera/Espera.Core/Management/LibraryFileWriter.cs
@@ -1,4 +1,5 @@
 ﻿using Rareform.Validation;
+using System;
 using System.Collections.Generic;
 using System.IO;
 
@@ -18,10 +19,18 @@ namespace Espera.Core.Management
 
         public void Write(IEnumerable<LocalSong> songs, IEnumerable<Playlist> playlists, string songSourcePath)
         {
-            using (FileStream targetStream = File.Create(this.targetPath))
+            // Create a temporary file, write the library it, then delete the original library file
+            // and rename our new one. This ensures that there is a minimum possibility of things
+            // going wrong, for example if the process is killed mid-writing.
+            string tempFilePath = Path.Combine(Path.GetDirectoryName(this.targetPath), Guid.NewGuid().ToString());
+
+            using (FileStream targetStream = File.Create(tempFilePath))
             {
                 LibraryWriter.Write(songs, playlists, songSourcePath, targetStream);
             }
+
+            File.Delete(this.targetPath);
+            File.Move(tempFilePath, this.targetPath);
         }
     }
 }
