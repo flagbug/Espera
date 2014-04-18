4382fb78473efea0dd52fb6a04704e37e6a4415e
Dennis Daume <daume.dennis@gmail.com>
2014-04-18 19:41:07 +0200

    The previous refactoring was stupid

--- Espera/Espera.Services/MobileApi.cs
+++ Espera/Espera.Services/MobileApi.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Reactive.Disposables;
 using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Reactive.Threading.Tasks;
@@ -79,11 +82,57 @@ namespace Espera.Services
         {
             byte[] message = Encoding.Unicode.GetBytes("espera-server-discovery");
 
-            using (var client = new UdpClient(this.port))
+            // For some reason, closing a UDPClient takes forever, so we keep them in a cache
+            // instead of recreating them every time
+            var clientCache = new List<UdpClient>();
+            var clientDisposable = new CompositeDisposable();
+
+            using (clientDisposable)
             {
                 while (!this.dispose)
                 {
-                    await client.SendAsync(message, message.Length, new IPEndPoint(IPAddress.Broadcast, this.port));
+                    await Task.Run(() =>
+                    {
+                        // Look up all IP addresses with every loop, incase we have a new network
+                        // adapter is added
+                        IPAddress[] addresses = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
+                        IEnumerable<IPAddress> localSubnets = addresses.Where(x => x.AddressFamily == AddressFamily.InterNetwork);
+
+                        // Get all intern networks and fire our discovery message on the last byte
+                        // up and down This is the only way to ensure that the clients can discover
+                        // the server reliably
+                        foreach (IPAddress ipAddress in localSubnets)
+                        {
+                            UdpClient client = clientCache.SingleOrDefault(x => ((IPEndPoint)x.Client.LocalEndPoint).Address.Equals(ipAddress));
+
+                            if (client == null)
+                            {
+                                client = new UdpClient(new IPEndPoint(ipAddress, this.port));
+                                clientCache.Add(client);
+                                clientDisposable.Add(client);
+                            }
+
+                            byte[] address = ipAddress.GetAddressBytes();
+
+                            // We don't use a broadcast here, but send a UDP packet to each address
+                            // in the same subnet individually, so the client can determine the
+                            // correct sender IP address.
+                            //
+                            // I don't know if we actually need this in the real world, but when
+                            // using the Genymotion emulator, which uses Virtualbox, a virtual
+                            // network adapter is created and the client on the emulator gets
+                            // confused which IP address it should choose.
+                            //
+                            // I'm not good at networking stuff, so I just assume all devices just
+                            // have a different last digit block.
+                            foreach (int i in Enumerable.Range(1, 254).Where(x => x != address[3]).ToList()) // Save to a list before we change the last address byte
+                            {
+                                address[3] = (byte)i;
+
+                                client.Send(message, message.Length, new IPEndPoint(new IPAddress(address), this.port));
+                            }
+                        }
+                    });
 
                     await Task.Delay(1000);
                 }
@@ -99,7 +148,6 @@ namespace Espera.Services
             {
                 listener.Start();
             }
-
             catch (SocketException ex)
             {
                 this.Log().ErrorException(string.Format("Port {0} is already taken", this.port), ex);
