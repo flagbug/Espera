ce50b37a74099e5b600f57ea2c417abb9011a767
Dennis Daume <daume.dennis@gmail.com>
2014-07-31 13:20:51 +0200

    Added a test

---
+++ Espera/Espera.Core.Tests/AudioPlayerTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Espera.Core.Audio;
+using NSubstitute;
+using Xunit;
+
+namespace Espera.Core.Tests
+{
+    public class AudioPlayerTest
+    {
+        [Fact]
+        public async Task StopsCurrentMediaPlayerWhenSwitchingAndPlaying()
+        {
+            var audioPlayer = new AudioPlayer();
+
+            var oldMediaPlayer = Substitute.For<IMediaPlayerCallback>();
+            var newMediaPlayer = Substitute.For<IMediaPlayerCallback>();
+
+            audioPlayer.RegisterAudioPlayerCallback(oldMediaPlayer);
+
+            var song = Helpers.SetupSongMock();
+
+            await audioPlayer.LoadAsync(song);
+            await audioPlayer.PlayAsync();
+
+            audioPlayer.RegisterAudioPlayerCallback(newMediaPlayer);
+
+            var song2 = Helpers.SetupSongMock();
+
+            await audioPlayer.LoadAsync(song2);
+            await audioPlayer.PlayAsync();
+
+            oldMediaPlayer.Received(1).StopAsync();
+        }
+    }
+}
