60df6e66212e18df906714ce1778957224528c22
Dennis Daume <daume.dennis@gmail.com>
2014-08-05 17:10:17 +0200

    Document the exception

--- Espera/Espera.Core/INetworkSongFinder.cs
+++ Espera/Espera.Core/INetworkSongFinder.cs
@@ -5,6 +5,10 @@ namespace Espera.Core
 {
     public interface INetworkSongFinder<T> where T : Song
     {
+        /// <summary>
+        /// Searches songs with the specified search term.
+        /// </summary>
+        /// <exception cref="NetworkSongFinderException">The search failed.</exception>
         Task<IReadOnlyList<T>> GetSongsAsync(string searchTerm);
     }
 }
