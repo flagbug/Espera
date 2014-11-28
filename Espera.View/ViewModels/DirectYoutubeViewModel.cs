3d1b1a67880b9afb1d82d69a67a72d74eae334e5
Dennis Daume <daume.dennis@gmail.com>
2014-11-28 23:50:07 +0100

    Fixed a typo

--- Espera.View/ViewModels/DirectYoutubeViewModel.cs
+++ Espera.View/ViewModels/DirectYoutubeViewModel.cs
@@ -35,7 +35,7 @@ namespace Espera.View.ViewModels
         /// <summary>
         /// Resolves the given YouTube URL and adds the song to the playlist.
         ///
-        /// This method will only execute successfuly if the <see cref="SongSourceViewModel{T}.AddToPlaylistCommand"/> command can execute.
+        /// This method will only execute successfully if the <see cref="SongSourceViewModel{T}.AddToPlaylistCommand"/> command can execute.
         /// </summary>
         public async Task AddDirectYoutubeUrlToPlaylist(Uri url, int? targetIndex)
         {
