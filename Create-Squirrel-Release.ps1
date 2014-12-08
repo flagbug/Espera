61ee498f85c9b91dbc011b51c5901302934e4d0e
Dennis Daume <daume.dennis@gmail.com>
2014-12-08 22:10:20 +0100

    Link the root shortcut to a batch file that calls the changing shortcut
    A shortcut that links to another shortcut gets the link of the second statically assigned so we have to use an intermediate batch file

--- Create-Squirrel-Release.ps1
+++ Create-Squirrel-Release.ps1
@@ -32,6 +32,7 @@ $SquirrelUpdate = Join-Path  $SquirrelPackagePath "tools\Squirrel.exe"
 $BuildPath = "$PSScriptRoot\Espera.View\bin\Release"
 $NuSpecPath = "$PSScriptRoot\Espera.nuspec"
 $ReleasesFolder = "$PSScriptRoot\SquirrelReleases"
+$Icon = "$PSScriptRoot\Espera.View\Images\ApplicationIcon.ico"
 
 # ==================================== NuSpec Metadata
 
@@ -111,6 +112,7 @@ cp $SquirrelFullNuPkgOutputPath "$PortableAppPath\packages"
 cp "$ReleasesFolder\RELEASES" "$PortableAppPath\packages"
 cp "$BuildPath" "$PortableAppPath\app-$Version" -Recurse
 cp $SquirrelUpdate "$PortableAppPath\Update.exe"
+cp $Icon "$PortableAppPath\Icon.ico"
 
 # Create the shortcut that Squirrel will update for us
 $WshShell = New-Object -comObject WScript.Shell
@@ -118,11 +120,16 @@ $Shortcut = $WshShell.CreateShortcut("$PortableAppPath\Espera.lnk")
 $Shortcut.TargetPath = "$PortableAppPath\app-$Version\Espera.exe"
 $Shortcut.Save()
 
-# Create a static shortcut to the shortcut we've just created, 
-# this is the one the user will use
+# Create a batch file to launch the shortcut we've just created, 
+# because we can't really link a shortcut to another shortcut
+"start Espera\Espera.lnk" | out-file -Encoding ascii "$PortableAppPath\Espera.cmd"
+
+# Create a static shortcut to the batch file we've just created, 
+# this shortcut is the one the user will use
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$PortableFolder\Espera.lnk")
-$Shortcut.TargetPath = "$PortableAppPath\Espera.lnk"
+$Shortcut.TargetPath = "$PortableAppPath\Espera.cmd"
+$Shortcut.IconLocation = "$PortableAppPath\Icon.ico"
 $Shortcut.Save()
 
 ZipFiles $ReleaseZip $PortableFolder
