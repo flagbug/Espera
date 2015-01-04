55157ffae2c708974fd6e3bd453363252d25a5cd
Dennis Daume <daume.dennis@gmail.com>
2015-01-04 01:38:19 +0100

    :arrow_up: Refit

--- Espera.Core/RefitStubs.cs
+++ Espera.Core/RefitStubs.cs
@@ -15,12 +15,14 @@ namespace RefitInternalGenerated
     [AttributeUsage (AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Enum | AttributeTargets.Constructor | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Event | AttributeTargets.Interface | AttributeTargets.Delegate)]
     sealed class PreserveAttribute : Attribute
     {
+#pragma warning disable 0649
         //
         // Fields
         //
         public bool AllMembers;
 
         public bool Conditional;
+#pragma warning restore 0649
     }
 }
 
