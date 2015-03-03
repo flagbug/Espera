291a906597d592724a7b03c14d7d4194891dd534
Dennis Daume <daume.dennis@gmail.com>
2015-03-03 20:20:18 +0100

    Initial hack on the Squirrel side of the build

--- build.fsx
+++ build.fsx
@@ -10,6 +10,12 @@ let buildDir = "./Espera.View/bin/Release"
 let releasesDir = "./Releases"
 let setupIcon = "./Espera.View/Images/ApplicationIcon.ico"
 let portableDir = releasesDir @@ "EsperaPortable"
+let squirrelPath = findToolFolderInSubPath "./packages/**/Squirrel.exe" null
+let squirrel = squirrelPath @@ "Squirrel.exe"
+let squirrelUpdate = squirrelPath @@ "Squirrel.exe"
+let nuspecPath = "./Espera.nuspec"
+let nuspecData = getNuspecProperties (ReadFileAsString nuspecPath)
+let nugetPackage = "./Espera." + nuspecData.Version + ".nupkg"
 
 Target "Clean" (fun _ ->
     CleanDirs [releasesDir; buildDir]
@@ -18,6 +24,13 @@ Target "Clean" (fun _ ->
 Target "Build" (fun _ ->
     MSBuildRelease null "Rebuild" [solution]
         |> Log "Build-Output: "
+)
+
+Target "Squirrel" (fun _ ->
+    let includeUpdater = getBuildParamOrDefault "includeUpdater" "false"
+    
+    if includeUpdater = "true" then
+        Copy squirrelUpdate buildDir
 )
 
 "Clean"
