b05c9a04634d03bee816deca70a89441b695a545
Dennis Daume <daume.dennis@gmail.com>
2015-10-27 18:39:43 +0100

    Revert ":arrow_up: Xamarin.Insights@1.10.2.110"
    This reverts commit 7138568efc663fe7c0c9fb3f6b7c3a702d9bf6d8.

--- Espera.Core/Analytics/XamarinAnalyticsEndpoint.cs
+++ Espera.Core/Analytics/XamarinAnalyticsEndpoint.cs
@@ -35,7 +35,7 @@ namespace Espera.Core.Analytics
 
         public void ReportFatalException(Exception exception)
         {
-            Insights.Report(exception, Insights.Severity.Critical);
+            Insights.Report(exception, ReportSeverity.Error);
             Insights.Save().Wait();
         }
 
